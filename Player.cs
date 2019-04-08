153fc57bc6066d6be497b4fecc75cf9ae22a4abe
brentjacobs1 <49160834+brentjacobs1@users.noreply.github.com>
2019-04-08 08:09:16 -0700

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,36 @@
+public float maxSpeed = 3;
+public float speed = 50f;
+public float jumpPower = 150f;
+private Animator anim;
+public bool grounded;
+
+private RigidBody2D rb2d;
+
+void Start()
+{
+rb2d = gameObject.GetComponent<RigidBody2D>();
+anim = gameObject.GetComponent<Animator>();
+}
+
+void Update()
+{
+anim.SetBool("Grounded",grounded);
+anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+}
+
+void FixedUpdate()
+{
+float h = Input.GetAxis("Horizontal");
+//Moving the player
+rb2d.AddForce((Vector2.right * speed) * h);
+//Limiting the speed of the player
+if (rb2d.velocity.x > maxSpeed)
+{
+rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+}
+
+if(rb2d.velocity.x < -maxSpeed)
+{
+rb2d.velocity = new Vector2(-maxSpeed, rb2d.velocity.y);
+}
+}
