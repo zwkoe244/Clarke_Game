8ab0434017538fbc7050cd03d1e7ad5bf64198dd
kpnau145 <49160838+kpnau145@users.noreply.github.com>
2019-04-12 08:23:40 -0700

    Update playerAttack.cs

--- playerAttack.cs
+++ playerAttack.cs
@@ -1,1 +1,40 @@
+private bool attacking = false;
 
+private float attackTimer = 0;
+private float attackCd = 0.3f;
+
+public Collider2D attackTrigger;
+
+private Animator anim;
+
+void Awake()
+{
+anim = gameObject.GetComponet<Animator>();
+attackTrigger.enabled = false;
+}
+
+void Update()
+{
+  if(Input.GetKeyDown("f") && !attacking)
+  {
+   attacking = true;
+   attackTimer = attackCd;
+   
+   attackTrigger.enabled = true;
+  }
+  
+  if(attacking)
+  {
+     if(attackTimer > 0)
+     {
+       attackTimer -= Time.deltaTime;
+     }
+     else
+     {
+       attacking = false;
+       attackTrigger.enabled = false;
+     }
+  }
+  
+  anim.SetBool("Attacking", attacking);
+}
